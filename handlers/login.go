d9f424e49c33b26de3a1ad6c8d3843d763dce46a
Mohamed Ahmed <motawfik1998@gmail.com>
2019-05-10 14:45:45 +0200

    Added the navbar

--- handlers/login.go
+++ handlers/login.go
@@ -16,9 +16,10 @@ type MyDB struct {
 func (db *MyDB) showLoginPage(c echo.Context) error {
 	status, message := getFlashMessages(&c)
 	return c.Render(http.StatusOK, "login.html", echo.Map{
-		"status":  status,
-		"message": message,
-		"title":   "تسجيل دخول",
+		"status":     status,
+		"message":    message,
+		"title":      "تسجيل دخول",
+		"hideNavBar": true,
 	})
 }
 
